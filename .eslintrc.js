247975121ced1c269a697fb6d29de57ee8d7e3fc
carlos <huaixian.huang@gmail.com>
2018-11-08 16:08:56 +0800

    Added: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    'no-console': 'off',
+    "indent": 0,
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    quotes: 0,
+    "semi": [
+      "error",
+      "always"
+    ],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+
+  }
+};
